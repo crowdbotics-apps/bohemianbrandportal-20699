b04e862e6206e0a27d7495a59757599c54d2764f
Crowdbotics <team@crowdbotics.com>
2020-09-25 22:03:32 +0000

    Update models

--- backend/bohemianbrandportal_20699/wsgi.py
+++ backend/bohemianbrandportal_20699/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bohemianbrandportal_20699.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bohemianbrandportal_20699.settings")
 
 application = get_wsgi_application()
